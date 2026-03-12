2998bece5ae38ca3e42b311559fe387e30214057
KeyBEOMM <kgbkelvin@naver.com>
2026-03-12 22:46:07 +0900

    WEEK1

--- build/cpp_pkg1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kim/HYPER_WS/src/cpp_pkg1
-# Build directory: /home/kim/HYPER_WS/build/cpp_pkg1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/cpphello/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kim/HYPER_WS/src/cpphello
-# Build directory: /home/kim/HYPER_WS/build/cpphello
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
